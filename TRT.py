1f321cc3735b60ee36087e460af2abdd70c226be
awan0177 <147627523+awan0177@users.noreply.github.com>
2023-11-17 12:41:35 +0500

    Update AWN.py

--- TRT.py
+++ TRT.py
@@ -1587,43 +1587,14 @@ def AWN6(ids,passlist):
 					break
 			else:
 				continue
-		self.loop+=1
-		
-		
-		def Subscraption():
-	clear()
-	print(logo)
-	r1=requests.get("https://github.com/awan0177/trick/blob/main/trick.txt").text
-	if key1 in r1:
-		os.system('clear')
-		print(logo)
-		Main()
-	else:
-		os.system("clear")
-		print(logo)
-		print("\t \033[1;32m First Get Approvel\033[1;37m ")
-		time.sleep(1)
-		os.system("clear")
-		print(logo)
-		print ("")
-		print(" \033[1;32m AWAN Toll Free BUT You Need Get Approved First\033[1;37m\n")
-		print(" \033[1;32m Note : AWAN FREE HA BHAIYO ENJOYA   \033[1;37m")
-		print ("")
-		print(" Your Key is Not Approved ")
-		print("")
-		print(" Copy And Send Key To Admin")
-		print ("")
-		print (" Your Key : "+ak+ah+key1 )
-		print ("")
-		name = input(" Your Name : ")
-		print ("")
-		gf = input(" Your gf Name : ")
-		print ("")
-		lol = input(" Your Your Email : ")
-		print ("")
-		input(" Press Enter To Send Key")
-		time.sleep(3.5)
-		tks = 'Dear%20Admin,%20Please%20Approved%20My%20Key%20To%20Premium%20%20Thanks%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20My%20Email%20:%20'+lol+'%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20My%20Name%20:%20'+name+'%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20My%20%20Key%20%20:%20'+ak+ah+key1
-		os.system('am start https://wa.me/+923461785228?text=' + tks)
-		Subscraption()        
-Subscraption()
+				loop+=1
+	except requests.exceptions.ConnectionError:
+		time.sleep(10)
+	except:
+		pass
+try:
+	menu()
+except requests.exceptions.ConnectionError:
+	print('\n No internet connection ...')
+	exit()
+except:exit
